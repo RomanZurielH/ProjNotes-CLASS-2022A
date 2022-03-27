53952532743b06b53cee044e27806ed49db842e5
RomanZurielH <betterthanyou24awesome@gmail.com>
2022-03-26 19:33:20 -0600

    adding babel-watch

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // render manda a renderizar (generar y entregar)
+  // la vista a el cliente (index = plantilla y  {title = Datos } )
+  let emojieDataset = ['😎','🗼','😏','☕','🙋‍♂️','👏','🤓','🖤','🎶','🎂','🐱‍👤'];
+  let emojie = emojieDataset[Math.floor(Math.random()* emojieDataset.length)];
+  res.render('index', 
+  // Este el el View_Model
+  { 
+    title: 'Express', 
+    author: 'Zuriel Román Hernández', 
+    emojie 
+  }); 
 });
 
 module.exports = router;
