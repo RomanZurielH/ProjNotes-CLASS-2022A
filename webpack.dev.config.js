b43e222d85f82b7c1dc726942993d2a06f1cd11b
RomanZurielH <betterthanyou24awesome@gmail.com>
2022-03-28 08:10:13 -0600

    Se agrega configuración de webpack para configuración

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -13,14 +13,4 @@ module.exports = {
     // 2.3 path publico
     publicPath: '/',
   },
-  // 3. Configurando el servidor de desarrollo
-  devServer: {
-    // 3.1 Folder de archivos estaticos
-    static: path.join(__dirname, "public"),
-    // 3.2 Puerto del servidor
-    // de WP (Webpack)
-    port: 8080,
-    //3.3 Definiendo host
-    host: "localhost",
-  }
-}
+};
