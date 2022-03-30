416751ba3eef25955e6b19162b910bc1c0b49990
RomanZurielH <betterthanyou24awesome@gmail.com>
2022-03-29 22:08:19 -0600

    babel configurado y CssExtract sin probar

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,56 @@
 // Importando dependencias path
 // dependencia del core Node
-const path = require("path");
+const path = require('path');
+// Plugins para Webpack
+const MiniCssExtractPlugin = requiere('mini-css-extract-plugin');
+
 module.exports = {
-  // 1. Especificar el archivo de entrada
-  entry: "/client/index.js",
-  // 2. Especificar el archivo de salida
-  output: {
-    // 2.1 Ruta absoluta de la salida
-    path: path.resolve(__dirname, "public"),
-    // 2.2 Nombre del archivo de salida
-    filename: path.join('javascripts','bundle.js'),
-    // 2.3 path publico
-    publicPath: '/',
-  },
-  // 3. Configurando el servidor de desarrollo
-  devServer: {
-    // 3.1 Folder de archivos estaticos
-    static: path.join(__dirname, "public"),
-    // 3.2 Puerto del servidor
-    // de WP (Webpack)
-    port: 8080,
-    //3.3 Definiendo host
-    host: "localhost",
-  }
-}
+    // 1. Especificar el archivo de entrada 
+    entry: '/client/index.js',
+    // 2. Especificar el archivo de salida
+    output:{
+        // 2.1 Ruta absoluta de la salida
+        path: path.resolve(__dirname, 'public'),
+        // 2.2 Nombre del archivo de salida
+        filename: path.join('javascripts','bundle.js'),
+        // 2.3 path publico 
+        publicPath: '/',
+    },
+    // 3. Modulos
+    module:{
+        rules:[
+            // 3.1 Regla para Babel
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    // 3.1.1 Primer stage
+                    {
+                        loader:'babel-loader',
+                        options: {
+                            presets:[
+                                [
+                                    '@babel/preset-env',{
+                                        modules: false,
+                                        useBuiltIns: 'usage',
+                                        targets: '> 0.25%, not dead',
+                                        corejs: 3,
+                                    }
+                                ]
+                            ]
+                        }
+                    }
+                ]
+            },
+            // 3.2 Reglas para Css
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader],
+            }
+        ]
+    },
+    // 4. Plugins
+    plugins: [ new MiniCssExtractPlugin({
+        filename: path.join('stylesheet','styles.css')
+    })]
+};
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,16 +1,56 @@
 // Importando dependencias path
 // dependencia del core Node
-const path = require("path");
+const path = require('path');
+
 module.exports = {
-  // 1. Especificar el archivo de entrada
-  entry: "/client/index.js",
-  // 2. Especificar el archivo de salida
-  output: {
-    // 2.1 Ruta absoluta de la salida
-    path: path.resolve(__dirname, "public"),
-    // 2.2 Nombre del archivo de salida
-    filename: path.join('javascripts','bundle.js'),
-    // 2.3 path publico
-    publicPath: '/',
-  },
+    // 1. Especificar el archivo de entrada 
+    entry: '/client/index.js',
+    // 2. Especificar el archivo de salida
+    output:{
+        // 2.1 Ruta absoluta de la salida
+        path: path.resolve(__dirname, 'public'),
+        // 2.2 Nombre del archivo de salida
+        filename: path.join('javascripts','bundle.js'),
+        // 2.3 path publico 
+        publicPath: '/',
+    },
+    // 3. Configurando el servidor de desarrollo
+    devServer:{
+        // 3.1 Folder de archivos estaticos
+        static: path.join(__dirname,'public'),
+        // 3.2 Puerto del servidor 
+        // de WP (Webpack)
+        port:8080,
+        //3.3 Definiendo host
+        host: 'localhost'
+    },
+    // 4. Modulos
+    module:{
+        rules:[
+            // 3.1 Regla para Babel
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    // 3.1.1 Primer stage
+                    {
+                        loader:'babel-loader',
+                        options: {
+                            presets:[
+                                [
+                                    '@babel/preset-env',{
+                                        modules: false,
+                                        useBuiltIns: 'usage',
+                                        targets: '> 0.25%, not dead',
+                                        corejs: 3,
+                                    }
+                                ]
+                            ]
+                        }
+                    }
+                ]
+            }
+        ]
+    }
 };
+
