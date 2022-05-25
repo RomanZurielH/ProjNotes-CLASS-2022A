7a8c3ba30c64a3cd221f14bfc4f330ee4904af63
RomanZurielH <betterthanyou24awesome@gmail.com>
2022-05-25 09:23:39 -0500

    S41 Agregando Menu Dropdwon de acciones

--- client/index.js
+++ client/index.js
@@ -7,9 +7,14 @@ import './styles/mystyle.css';
 
 /* Inicializando elementos de materializecss */
 document.addEventListener('DOMContentLoaded', () => {
+  /* Iniciailizando los sideNavas */
   // Obteniendo la referencia de la barra de navegacion
   // lateral
   const sideNavs = document.querySelectorAll('.sidenav');
   // eslint-disable-next-line no-undef
   M.Sidenav.init(sideNavs);
+
+  /* Inicializando los dropdown */
+  const dropdowns = document.querySelectorAll('.dropdown-trigger');
+  M.Dropdown.init(dropdowns);
 });
